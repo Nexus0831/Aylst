8cc7b24f7bed5975cfcbcaef735cfa40a24b6ed0
Nexus0831 <pride0831@gmail.com>
2022-09-14 13:40:00 +0900

    feat: ネオンのUIを実装

---
+++ src/components/ChemicalElement.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="element">
+    <span class="number">
+      79
+    </span>
+    <span class="symbol">
+      Au
+    </span>
+    <span class="name">
+      Aurum
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class ChemicalElement extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+  .element
+    blur = 1.75rem
+    box-blur = "calc(0.5 * %s)" % blur
+    glow = #fcdb65
+    size = 128px
+
+    align-items center
+    border-radius 12px
+    border 4px solid currentColor
+    box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.15),
+        0 0 0 2px rgba(0, 0, 0, 0.15),
+      inset 0 0 box-blur glow,
+        0 0 box-blur glow
+    color glow
+    display: inline-flex;
+    flex-direction: column;
+    /*font-family: system-ui, sans-serif;*/
+    height: size;
+    justify-content: space-around;
+    padding: 1rem;
+    width: size;
+
+    .number
+      font-weight 700
+
+    .symbol
+
+      color #000000
+      -webkit-text-stroke 3px glow
+      font-size 4rem
+      text-shadow 0 0 blur glow
+      filter saturate(175%)
+</style>
--- src/components/view/HomeView.vue
+++ src/components/view/HomeView.vue
@@ -2,19 +2,33 @@
   <div id="home">
     <div class="main-content">
       <div class="content-title">
-        This is Aylst.
+        <span class="neon-title">
+          This is Aylst.
+        </span>
         <br/>
-        Where my efforts converge.
-        <span class="content-body">This site contains a UI that I created for study purposes.</span>
+        <span class="neon-text">
+          Where my efforts converge.
+        </span>
+        <span class="content-body">
+          This site contains a UI that I created for study purposes.
+        </span>
       </div>
+    </div>
+    <div class="periodic-table">
+      <ChemicalElement/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ChemicalElement from '@/components/ChemicalElement.vue';
 
-@Component({})
+@Component({
+  components: {
+    ChemicalElement,
+  },
+})
 export default class HomeView extends Vue {
 }
 </script>
@@ -40,10 +54,7 @@ export default class HomeView extends Vue {
     /** position end **/
   .content-title
     /** style start **/
-    background-color #6155a9
-    color #fcdb65
-    font-size 64px
-    font-weight bold
+    background-color #101010
     height 100%
     /** style end **/
 
@@ -58,6 +69,37 @@ export default class HomeView extends Vue {
     grid-row 1 / 2
     grid-column 1 / 4
     /** position end **/
+
+  .neon-title
+    blur = 1.75rem
+    box-blur = calc(0.5 * blur)
+    glow = #fcdb65
+
+    color #000000
+    -webkit-text-stroke 3px glow
+    font-size 64px
+    text-shadow 0 0 blur glow
+    filter saturate(175%)
+
+  .neon-text
+    neon-text-color = #fcdb65
+    animation flicker 1.5s infinite alternate
+    font-size 24px
+
+    @keyframes flicker
+      0%, 19%, 21%, 23%, 25%, 54%, 56%, 100%
+        text-shadow: -0.2rem -0.2rem 1rem #fff,
+          0.2rem 0.2rem 1rem #fff,
+          0 0 2rem neon-text-color,
+          0 0 4rem neon-text-color,
+          0 0 6rem neon-text-color,
+          0 0 8rem neon-text-color,
+          0 0 10rem neon-text-color
+
+      20%, 24%, 55%
+        text-shadow none
+        /*box-shadow none*/
+
   .content-body
     /** style start **/
     padding 12px
@@ -69,5 +111,12 @@ export default class HomeView extends Vue {
     /** layout end **/
 
     /** position start **/
+    /** position end **/
+  .periodic-table
+    padding 12px
+    background-color #303030
+    /** position start **/
+    grid-row 3 / 4
+    grid-column 1 / 2
     /** position end **/
 </style>
